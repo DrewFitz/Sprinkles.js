c133531022a06ebcb5198b3a64a35ae5f2bcc113
Drew Fitzpatrick <socialengineerdude@gmail.com>
2012-04-25 23:30:18 -0400

    Moved particle system into self-containted file

--- program.js
+++ program.js
@@ -1,101 +1,3 @@
-// Particle system
-// 
-// Just a little particle system written in javascript for fun
-//
-//
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
-function Particle() { 
-  this.x; 
-  this.y;
-  this.width = 20;
-  this.height = 20;
-  this.vX = (Math.random() * 10) - 5;
-  this.vY = (Math.random() * 10) - 5;
-  this.isVisible = function() {
-    if (this.x > canvas.clientWidth) 
-      return false;
-    else if (this.x < 0)
-      return false;
-    else if (this.y > canvas.clientHeight)
-      return false;
-    else if (this.y < 0)
-      return false;
-
-    return true;
-  };
-  this.fill = "";
-  this.draw = function() {
-    if (this.fill !== "") {
-      ctx.fillStyle = this.fill;
-    }
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
-}
-
-function ParticleManager() {
-  if (!(this instanceof ParticleManager)) {
-    return new ParticleManager();
-  }
-  var particles = [];
-  var globalXAcc = 0;
-  var globalYAcc = -2;
-
-  this.setGlobalAcceleration = function(x,y){
-    globalXAcc = x;
-    globalYAcc = y;
-  }
-  this.origin = {
-    x: canvas.clientWidth / 2,
-    y: canvas.clientHeight / 2
-  };
-  this.active = true;
-  this.drawOrigin = false;
-  this.particleFill = "";
-
-  this.count = function() {
-    return particles.length;
-  };
-  this.update = function() {
-    if (this.active) {
-      var newParticle = new Particle();
-      newParticle.x = this.origin.x;
-      newParticle.y = this.origin.y;
-      newParticle.fill = this.particleFill;
-      particles.push(newParticle);
-    }
-    particles.forEach(function(particle, index, particles) {
-      particle.vX += globalXAcc;
-      particle.vY += globalYAcc;
-      particle.x += particle.vX;
-      particle.y += particle.vY;
-      if(!(particle.isVisible())){
-        particles.splice(index, 1);
-      }
-    });
-  };
-  function drawCross(x,y) {
-    ctx.fillStyle = "";
-    ctx.strokeStyle = "rgb(0,0,200)";
-    ctx.beginPath();
-    ctx.moveTo(x - 5, y);
-    ctx.lineTo(x + 5, y);
-    ctx.moveTo(x, y - 5);
-    ctx.lineTo(x, y + 5);
-    ctx.closePath();
-    ctx.stroke();
-  };
-  this.render = function() {
-    ctx.fillStyle = "rgb(200,0,0)";
-    particles.forEach(function(particle) {
-      particle.draw();
-    });
-    if(this.drawOrigin)
-      drawCross(this.origin.x, this.origin.y);
-  };
-}
 
 function clear() {
   ctx.fillStyle = "rgba(0,0,0,0.2)";
@@ -136,8 +38,10 @@ var logInterval = setInterval(function() {
 var toggleInterval = setInterval(function() {
   if(pm.active) {
     pm.active = false;
+    pm2.active = true;
   } else {
     pm.active = true;
+    pm2.active = false;
   }
 }, 1000);
 
